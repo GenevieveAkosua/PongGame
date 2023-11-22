4d4d02a8cc636eb901f7ff7de203b15059eb8781
Génevieve Chikwanha <genevievechikwanha@Genevieves-MacBook-Pro.local>
2023-11-22 23:27:06 +0200

    Adding scoreboards

--- Pong.py
+++ Pong.py
@@ -19,6 +19,8 @@ PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
 BALL_RADIUS = 7
 
+SCORE_FONT = pygame.font.SysFont("Helvetica", 40)
+
 
 class Paddle:
     COLOUR = WHITE
@@ -45,8 +47,8 @@ class Ball:
     COLOUR = WHITE
 
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
+        self.x = self.original_x = x
+        self.y = self.original_y = y
         self.radius = radius
         self.x_vel = self.MAX_VEL
         self.y_vel = 0
@@ -58,9 +60,20 @@ class Ball:
         self.x += self.x_vel
         self.y += self.y_vel
 
+    def reset(self):
+        self.x = self.original_x
+        self.y = self.original_y
+        self.y_vel = 0
+        self.x_vel *= -1
 
-def draw(window, paddles, ball):
+
+def draw(window, paddles, ball, left_score, right_score):
     window.fill(BLACK)
+
+    left_score_text = SCORE_FONT.render(f"{left_score}", 1, WHITE)
+    right_score_text = SCORE_FONT.render(f"{right_score}", 1, WHITE)
+    window.blit(left_score_text, (WIDTH//4 - left_score_text.get_width()//2, 20))
+    window.blit(right_score_text, (WIDTH * (3/4) - right_score_text.get_width()//2, 20))
 
     for paddle in paddles:
         paddle.draw(window)
@@ -73,6 +86,35 @@ def draw(window, paddles, ball):
     ball.draw(window)
 
     pygame.display.update()
+
+
+def collision(ball, left_paddle, right_paddle):
+    if ball.y + ball.radius >= HEIGHT:
+        ball.y_vel *= -1
+    elif ball.y - ball.radius <= 0:
+        ball.y_vel *= -1
+
+    if ball.x_vel < 0:
+        if left_paddle.y <= ball.y <= left_paddle.y + left_paddle.height:
+            if ball.x - ball.radius <= left_paddle.x + left_paddle.width:
+                ball.x_vel *= -1
+
+                middle_y = left_paddle.y + left_paddle.height / 2
+                diff_in_y = middle_y - ball.y
+                reduction_factor = (left_paddle.height / 2) / ball.MAX_VEL
+                y_vel = diff_in_y / reduction_factor
+                ball.y_vel = y_vel * -1
+
+    else:
+        if right_paddle.y <= ball.y <= right_paddle.y + right_paddle.height:
+            if ball.x + ball.radius >= right_paddle.x:
+                ball.x_vel *= -1
+
+                middle_y = right_paddle.y + right_paddle.height / 2
+                diff_in_y = middle_y - ball.y
+                reduction_factor = (right_paddle.height / 2) / ball.MAX_VEL
+                y_vel = diff_in_y / reduction_factor
+                ball.y_vel = y_vel * -1
 
 
 def paddle_movement(keys, left_paddle, right_paddle):
@@ -97,9 +139,12 @@ def main():
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     ball = Ball(WIDTH//2, HEIGHT//2, BALL_RADIUS)
 
+    left_score = 0
+    right_score = 0
+
     while run:
         clock.tick(FPS)
-        draw(WINDOW, [left_paddle, right_paddle], ball)
+        draw(WINDOW, [left_paddle, right_paddle], ball, left_score, right_score)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -110,6 +155,14 @@ def main():
         paddle_movement(keys, left_paddle, right_paddle)
 
         ball.move()
+        collision(ball, left_paddle, right_paddle)
+
+        if ball.x < 0:
+            right_score += 1
+            ball.reset()
+        elif ball.x > WIDTH:
+            left_score += 1
+            ball.reset()
 
     pygame.quit()
 
